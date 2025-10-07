32832fc57673000747b4ea45992755687433d9c3
SamU <samuel.ernesto.cuen@gmail.com>
2025-10-07 09:30:18 -0700

    Update email.py

--- Unidad 2/Spam/email.py
+++ Unidad 2/Spam/email.py
@@ -1,1 +1,14 @@
+# ---------------------------
+# 1) Representación del correo
+# ---------------------------
 
+class Email:
+    subject: str
+    sender: str
+    body: str
+    attachments: List[str]
+    label: str
+
+    def urls(self) -> List[str]:
+        url_regex = r"(https?://[^\s]+)"
+        return re.findall(url_regex, self.body, flags=re.I)
